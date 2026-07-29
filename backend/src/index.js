76709fba057e00b5dd37cfae3519debb6b11015e
Tanishka Makode <makodetanishka@gmail.com>
2026-07-29 23:25:47 +0530

    Configure webhook

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,12 +7,15 @@ import cors from 'cors';
 import fs from "fs"
 import path from "path"
 import job from "./lib/cron.js";
+import clerkWebhook from "./webhooks/clerk.webhook.js"
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
 const publicDir = path.join(process.cwd(), "public")
+
+app.use('/api/webhooks/clerk', express.raw({type:'application/json'}), clerkWebhook)
 
 app.use(express.json())
 app.use(cors({origin: FRONTEND_URL, credentials: true}));
